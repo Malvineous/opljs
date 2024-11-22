006fbfc13f22100a84df5c8dab420c3fb357d9c4
David Konsumer <konsumer@jetboystudio.com>
2024-11-22 00:44:37 -0800

    add wave function

--- index.js
+++ index.js
@@ -144,5 +144,64 @@ export default class OPL
 	write(reg, val) {
 		this.opl.write(reg, val);
 	}
+
+	/**
+	 * Output bytes for wav-output
+	 * 
+	 * @param {ArrayBuffer/Uint8Array/Buffer} bytes
+	 *    The bytes of the input (.imf, .wlf) file
+	 * 
+	 * @param {Boolean} wlf
+	 *   Trigger wlf file-mode (a bit faster)
+	 */
+	async wave(bytes, wlf=false) {
+	  const imf = new Uint8Array(bytes)
+	  const tempoInHz = wlf ? 700 : 560
+	  const samplesPerTick = Math.round(this.sampleRate / tempoInHz)
+	  const wavDataOut = []
+	  let lenIMF = imf.length
+	  let p = 0
+	  if (imf[0] || imf[1]) {
+	    lenIMF = imf[0] | (imf[1] << 8)
+	    p = 2
+	  }
+	  let samplesWritten = 0
+	  while (p < lenIMF) {
+	    const reg = imf[p + 0]
+	    const val = imf[p + 1]
+	    const delay = imf[p + 2] | (imf[p + 3] << 8)
+	    this.write(reg, val)
+	    if (delay) {
+	      let lenGen = delay * samplesPerTick
+	      while (lenGen > 0) {
+	        const lenNow = Math.max(2, Math.min(512, lenGen))
+	        wavDataOut.push(...this.generate(lenNow))
+	        samplesWritten += lenNow
+	        lenGen -= lenNow
+	      }
+	    }
+	    p += 4
+	  }
+	  const lenData = samplesWritten * 4
+	  const lenRIFF = lenData + 36
+	  const a = new ArrayBuffer(44 + lenData)
+	  const u = new Uint8Array(a)
+	  const v = new DataView(a)
+	  u.set(wavDataOut, 44)
+	  v.setUint32(0, 0x52494646); // RIFF
+	  v.setUint32(4, lenRIFF, true);
+	  v.setUint32(8, 0x57415645); // WAVE
+	  v.setUint32(12, 0x666d7420); // fmt_
+	  v.setUint32(16, 16, true); // fmt chunk length
+	  v.setUint16(20, 1, true); // 1=PCM
+	  v.setUint16(22, 2, true); // stereo
+	  v.setUint32(24, this.sampleRate, true);
+	  v.setUint32(28, this.sampleRate * this.channelCount * 2, true);
+	  v.setUint16(32, this.channelCount * 2, true); // block align
+	  v.setUint16(34, 16, true); // bits per sample
+	  v.setUint32(36, 0x64617461); // data
+	  v.setUint32(40, lenData, true); // data chunk length
+	  return u
+	}
 }
 
